eb308fd0d8047c6284bc30d25c1f2a243c6eb936
blacksun1 <simon@blacksun.cx>
2017-04-07 16:57:04 +0930

    Third commit

--- index.js
+++ index.js
@@ -1,86 +1,83 @@
+const Promise = require('bluebird').Promise;
+
 const _ = require('lodash');
-const Sonos = require('node-sonos');
-
+const assert = require('assert');
+const constants = require('./constants');
+const Sonos = Promise.promisifyAll(require('node-sonos'));
 console.log('Searching for Sonos devices...');
-const search = Sonos.search();
 
-const BLACKLIST = [
-    'Pnau',
-    'Lorde',
-    'Clean Bandit',
-    'Bliss n Eso',
-    'Calvin Harris',
-    'Maroon 5',
-    'Mura Masa',
-    'Nevada',
-    'Snakehips',
-    'Robin Schulz',
-    'Jon Bellion',
-    'Starley',
-    'The Chainsmokers',
-    'Cheat Codes',
-    'Sofi Tukker',
-    'Peking Duk',
-    'Martin Jensen',
-    'The Vamps',
-    'ODESZA',
-    'Isaiah',
-    'Ed Sheeran',
-    'Flume',
-    'Naughty Boy',
-    'Martin Solveig',
-    'Mr. Probz',
-    'L D R U',
-    'Chance The Rapper',
-    'Bebe Rexha',
-    'Rudimental',
-    'Dillon Francis',
-    'Zara Larsson',
-    'Tom Jay Williams',
-    'Martin Garrix',
-    'Steve Aoki',
-    'Ed Sheeran',
-    'PON CHO',
-    'FRENSHIP',
-    'Drake',
-    'Bruno Mars',
-    'Jax Jones',
-    'NERVO',
-    'Milky Chance',
-    'Vice',
-    'Sage The Gemini',
-    'San Holo',
-    'Olivia Holt',
-    'Katy Perry',
-    'Cosmo\'s Midnight',
-    'Gryffin',
-    'Marky Style'
-];
 
-search.on('DeviceAvailable', function (device, model) {
-    console.log(device, model)
-    //search.destroy();
+let search;
 
-    device.getQueue(function (err, resp) {
-        //console.log(err, resp);
+function checkQueue(device, err, resp) {
 
-        const blacklisted = _(resp.items)
-                            .map(t => t.artist)
-                            .uniq()
-                            .intersection(BLACKLIST)
-                            .value();
+    if (err) {
+        handleError('getQueue', err);
+    }
 
-        if (!blacklisted.length) {
-            console.log('All good');
-            process.exit();
-            return;
+    console.log('\n** QUEUE **\n');
+    _.each(resp.items, t => console.log(`Title:  ${t.title}\nArtist: ${t.artist}`));
+
+    const blacklisted = _(resp.items)
+                        .map(t => t.artist)
+                        .uniq()
+                        .intersection(constants.blacklist)
+                        .value();
+
+    if (!blacklisted.length) {
+        console.log('\nAll good');
+        return;
+    }
+
+    console.log('\nNuke queue');
+    device.flush((err) => {
+        if (err) {
+            console.error('device.flush done but with an error');
+        } else {
+            console.log('Nuke queue done!');
+        }
+    });
+}
+
+function deviceAvailable(device, model) {
+
+    console.log(`A device has been found: ${device.host}:${device.port}`);
+
+    device.getCurrentState(function (err, state) {
+
+        if (err) {
+            handleError('getCurrentState', err);
         }
 
-        console.log('Nuke queue');
-        device.flush((err) => {
-            console.log('Nuke queue done!');
-            process.exit();
-        });
-
+        if (state === 'stopped') {
+            console.log('Sonos is in the stopped state');
+        } else {
+            device.getQueue(checkQueue.bind(this, device));
+        }
     });
-});
+}
+
+function runSearch() {
+
+    if (search) {
+        search.destroy();
+    }
+
+    console.log('Doing a search');
+
+    search = Sonos.search();
+
+    search.on('error', handleError.bind(null, 'Search'));
+
+    search.on('DeviceAvailable', deviceAvailable);
+}
+
+function handleError(errorType, err) {
+
+    console.error(`${errorType} Error`, err);
+    process.exit(1);
+}
+
+setImmediate(runSearch, 5000);
+const myInterval = setInterval(runSearch, 5000);
+
